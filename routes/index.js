8929fd7a13c62f8d7227b6aa5173563f8b55add6
Jakob Lierman <jakob.lierman@gmail.com>
2018-10-26 00:33:17 +0200

    Update packages and first GET request

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('Ally backend server is up and running.');
+router.get('/API', function (req, res, next) {
+    res.send('Ally backend server is up and running.');
 });
 
 module.exports = router;
