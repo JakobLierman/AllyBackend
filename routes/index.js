8e5b9213d0f15faac3c06bdecbd6a0355aa1cea1
Jakob Lierman <jakob.lierman@gmail.com>
2018-10-31 10:47:27 +0100

    Added auth secret

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ let router = express.Router();
 let mongoose = require('mongoose');
 let jwt = require('express-jwt');
 
+// Auth secret
+let auth = jwt({
+    secret: process.env.ALLY_BACKEND_SECRET
+});
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.send('Ally backend server is up and running.');
